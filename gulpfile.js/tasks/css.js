6fb45c28d3cf99518874b635bf7887e4c1a75c25
nowgoant <631359926@qq.com>
2015-12-26 12:28:01 +0800

    remove gulp-sass then add gulp-compass

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,18 +1,18 @@
-var config = require('../config')
-if (!config.tasks.css) return
+var config = require('../config');
+if (!config.tasks.css) return;
 
-var gulp = require('gulp')
-var browserSync = require('browser-sync')
-var compass = require('gulp-compass')
-var sourcemaps = require('gulp-sourcemaps')
-var handleErrors = require('../lib/handleErrors')
-var autoprefixer = require('gulp-autoprefixer')
-var path = require('path')
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var compass = require('gulp-compass');
+var sourcemaps = require('gulp-sourcemaps');
+var handleErrors = require('../lib/handleErrors');
+var autoprefixer = require('gulp-autoprefixer');
+var path = require('path');
 
 var paths = {
   src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
   dest: path.join(config.root.dest, config.tasks.css.dest)
-}
+};
 
 gulp.task('css', function () {
   return gulp.src(paths.src)
